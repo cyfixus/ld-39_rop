dec8c9ae3735aa323b2a009ab76dde93811e0e01
cyfixus <seankfost@gmail.com>
2017-07-29 10:35:23 -0600

    yt link

--- game/scripts/world.gd
+++ game/scripts/world.gd
@@ -1,7 +1,22 @@
 extends Node2D
 
+onready var recharge = get_node("recharge")
 func _ready():
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
 	pass
+
+
+func _on_hq_body_enter( body ):
+	if body.get_groups().has("commander"):
+		recharge.start()
+
+
+func _on_recharge_timeout():
+	global.change_power(10)
+
+
+func _on_hq_body_exit( body ):
+	if body.get_groups().has("commander"):
+		recharge.stop()
