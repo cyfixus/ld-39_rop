e4868e86c66146333e1c5b85352f389a9df96031
cyfixus <seankfost@gmail.com>
2017-07-29 02:41:03 -0600

    batteries and light collision

--- game/scripts/battery.gd
+++ game/scripts/battery.gd
@@ -1,15 +1,7 @@
 extends Area2D
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
-
+	add_to_group("battery")
+	
 func _on_battery_body_enter( body ):
 	if body.get_groups().has("commander"):
 		global.change_power(10)
--- game/scripts/light_source.gd
+++ game/scripts/light_source.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var light = get_node("light")
 onready var power_loss = get_node("power_loss")
+onready var light_area = get_node("light_area")
 
 func _ready():
 	power_loss.start()
@@ -9,11 +10,12 @@ func _ready():
 	global.connect("game_over", self, "game_over")
 	
 func adjust_light(power):
-	var scale = power/10.0
+	var scale = power/20.0
 	if scale <= 1:
 		print("max")
 	else:
 		light.set_texture_scale(scale)
+		light_area.set_scale(Vector2(scale, scale))
 	print("scale", scale)
 	
 func game_over():
@@ -22,3 +24,13 @@ func game_over():
 func _on_power_loss_timeout():
 	global.change_power(-1)
 	power_loss.start()
+
+
+func _on_Area2D_area_enter( area ):
+	if area.get_groups().has("battery"):
+		area.show()
+
+
+func _on_Area2D_area_exit( area ):
+	if area.get_groups().has("battery"):
+		area.hide()
